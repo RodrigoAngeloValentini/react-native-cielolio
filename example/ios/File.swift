f2727d4af0e6df5ab2fd11d77ffeccf81103b885
Rodrigo Angelo Valentini <rodrigo.valentini@desbravador.com.br>
2024-08-23 17:10:12 -0300

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  CielolioExample
+//
+
+import Foundation
---
+++ ios/Cielolio.swift
@@ -0,0 +1,8 @@
+@objc(Cielolio)
+class Cielolio: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
